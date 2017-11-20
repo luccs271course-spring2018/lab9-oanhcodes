e8547198e1ae4b7274de2f417042630ec823b078
Linda Ho <linda.oanh.ho@gmail.com>
2017-11-20 16:48:59 -0600

    Complete methods to pass testEmpty, testContainsKey, testContainsValue, testRemove, and testNonEmpty tests

--- src/main/java/edu/luc/cs271/myhashmap/MyHashMap.java
+++ src/main/java/edu/luc/cs271/myhashmap/MyHashMap.java
@@ -3,159 +3,217 @@ package edu.luc.cs271.myhashmap;
 import java.util.*;
 
 /**
- * A generic HashMap custom implementation using chaining. Concretely, the table is an ArrayList of
- * chains represented as LinkedLists.
- *
- * @param <K> the key type
- * @param <V> the value type
- */
+* A generic HashMap custom implementation using chaining. Concretely, the table is an ArrayList of
+* chains represented as LinkedLists.
+*
+* @param <K> the key type
+* @param <V> the value type
+*/
 public class MyHashMap<K, V> implements Map<K, V> {
 
-  private static final int DEFAULT_TABLE_SIZE = 11; // a prime
+private static final int DEFAULT_TABLE_SIZE = 11; // a prime
 
-  private List<List<Entry<K, V>>> table;
+private List<List<Entry<K, V>>> table;
 
-  public MyHashMap() {
-    this(DEFAULT_TABLE_SIZE);
+public MyHashMap() {
+  this(DEFAULT_TABLE_SIZE);
+}
+
+public MyHashMap(final int tableSize) {
+  // allocate a table of the given size
+  table = new ArrayList<>(tableSize);
+  // then create an empty chain at each position
+  for (int i = 0; i < tableSize; i += 1) {
+    table.add(new LinkedList<>());
+  }
+}
+
+@Override
+public int size() {
+  // Done add the sizes of all the chains
+  int result = 0;
+
+  for (int i = 0; i < DEFAULT_TABLE_SIZE; i++){
+    result += table.get(i).size();
   }
 
-  public MyHashMap(final int tableSize) {
-    // allocate a table of the given size
-    table = new ArrayList<>(tableSize);
-    // then create an empty chain at each position
-    for (int i = 0; i < tableSize; i += 1) {
-      table.add(new LinkedList<>());
+  return result;
+}
+
+@Override
+public boolean isEmpty() {
+  return size() == 0;
+}
+
+@Override
+public boolean containsKey(final Object key) {
+  // Done follow basic approach of remove below (though this will be much simpler)
+  final int index = calculateIndex(key);
+  final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+
+  while (iter.hasNext()) {
+    final Entry<K, V> entry = iter.next();
+    if (entry.getKey().equals(key)) {
+      return true;
     }
   }
 
-  @Override
-  public int size() {
-    // TODO add the sizes of all the chains
-    int result = 0;
+  return false;
+}
 
+@Override
+public boolean containsValue(final Object value) {
+  // Done follow basic approach of remove below (though this will be much simpler)
 
-    return result;
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return size() == 0;
-  }
-
-  @Override
-  public boolean containsKey(final Object key) {
-    // TODO follow basic approach of remove below (though this will be much simpler)
-    final int index = calculateIndex(key);
-
-
-    return false;
-  }
-
-  @Override
-  public boolean containsValue(final Object value) {
-    // TODO follow basic approach of remove below (though this will be much simpler)
-
-
-    return false;
-  }
-
-  @Override
-  public V get(final Object key) {
-    // TODO follow basic approach of remove below (though this will be simpler)
-    final int index = calculateIndex(key);
-
-
-    return null;
-  }
-
-  @Override
-  public V put(final K key, final V value) {
-    // TODO follow basic approach of remove below (this will be similar)
-    final int index = calculateIndex(key);
-
-
-    return null;
-  }
-
-  @Override
-  public V remove(final Object key) {
-    final int index = calculateIndex(key);
-    final Iterator<Entry<K, V>> iter = table.get(index).iterator();
-    while (iter.hasNext()) {
-      final Entry<K, V> entry = iter.next();
-      if (entry.getKey().equals(key)) {
-        final V oldValue = entry.getValue();
-        iter.remove();
-        return oldValue;
+  for (int i = 0; i < DEFAULT_TABLE_SIZE; i++){
+    final Iterator<Entry<K, V>> currChain = table.get(i).iterator();
+    while (currChain.hasNext()) {
+      final Entry<K, V> entry = currChain.next();
+      if (entry.getValue().equals(value)) {
+        return true;
       }
     }
-    return null;
   }
 
-  @Override
-  public void putAll(final Map<? extends K, ? extends V> m) {
-    // TODO add each entry in m's entrySet
+  return false;
+}
 
+@Override
+public V get(final Object key) {
+  // Done follow basic approach of remove below (though this will be simpler)
+  final int index = calculateIndex(key);
 
-  }
-
-  @Override
-  public void clear() {
-    // TODO clear each chain
-
-
-  }
-
-  /** The resulting keySet is not "backed" by the Map, so we keep it unmodifiable. */
-  @Override
-  public Set<K> keySet() {
-    final Set<K> result = new HashSet<>();
-    // TODO populate the set
-
-
-    return Collections.unmodifiableSet(result);
-  }
-
-  /** The resulting values collection is not "backed" by the Map, so we keep it unmodifiable. */
-  @Override
-  public Collection<V> values() {
-    final List<V> result = new LinkedList<>();
-    // TODO populate the list
-
-
-    return Collections.unmodifiableCollection(result);
-  }
-
-  /** The resulting entrySet is not "backed" by the Map, so we keep it unmodifiable. */
-  @Override
-  public Set<Entry<K, V>> entrySet() {
-    final Set<Entry<K, V>> result = new HashSet<>();
-    // TODO populate the set
-
-
-    return Collections.unmodifiableSet(result);
-  }
-
-  @Override
-  public String toString() {
-    // TODO return the string representation of the underlying table
-    return "";
-  }
-
-  public boolean equals(final Object that) {
-    if (this == that) {
-      return true;
-    } else if (!(that instanceof Map)) {
-      return false;
-    } else {
-      // TODO simply compare the entry sets
-      return false;
+  final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+  while (iter.hasNext()) {
+    final Entry<K, V> entry = iter.next();
+    if (entry.getKey().equals(key)) {
+      entry.getValue();
     }
   }
 
-  private int calculateIndex(final Object key) {
-    // positive remainder (as opposed to %)
-    // required in case hashCode is negative!
-    return Math.floorMod(key.hashCode(), table.size());
+  return null;
+}
+
+@Override
+public V put(final K key, final V value) {
+  // Done follow basic approach of remove below (this will be similar)
+  final int index = calculateIndex(key);
+  final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+  while (iter.hasNext()) {
+    final Entry<K, V> entry = iter.next();
+    if (entry.getKey().equals(key)) {
+      final V oldValue = entry.getValue();
+      entry.setValue(value);
+      return oldValue;
+    } else {
+    }
   }
+  //If key is not found, add a new entry to the list
+  table.get(index).add(new AbstractMap.SimpleEntry<K, V>(key, value));
+  return null;
+}
+
+@Override
+public V remove(final Object key) {
+  final int index = calculateIndex(key);
+  final Iterator<Entry<K, V>> iter = table.get(index).iterator();
+  while (iter.hasNext()) {
+    final Entry<K, V> entry = iter.next();
+    if (entry.getKey().equals(key)) {
+      final V oldValue = entry.getValue();
+      iter.remove();
+      return oldValue;
+    }
+  }
+  return null;
+}
+
+@Override
+public void putAll(final Map<? extends K, ? extends V> m) {
+  // TODO add each entry in m's entrySet
+
+
+}
+
+@Override
+public void clear() {
+  // TODO clear each chain
+
+
+}
+
+/** The resulting keySet is not "backed" by the Map, so we keep it unmodifiable. */
+@Override
+public Set<K> keySet() {
+  final Set<K> result = new HashSet<>();
+  // Done populate the set
+  for (int i = 0; i < DEFAULT_TABLE_SIZE; i++){
+    final Iterator<Entry<K, V>> currChain = table.get(i).iterator();
+    while (currChain.hasNext()) {
+      final Entry<K, V> entry = currChain.next();
+      result.add(entry.getKey());
+    }
+
+  }
+
+  return Collections.unmodifiableSet(result);
+}
+
+/** The resulting values collection is not "backed" by the Map, so we keep it unmodifiable. */
+@Override
+public Collection<V> values() {
+  final List<V> result = new LinkedList<>();
+  // Done populate the list
+  for (int i = 0; i < DEFAULT_TABLE_SIZE; i++){
+    final Iterator<Entry<K, V>> currChain = table.get(i).iterator();
+    while (currChain.hasNext()) {
+      final Entry<K, V> entry = currChain.next();
+      result.add(entry.getValue());
+    }
+
+  }
+
+  return Collections.unmodifiableCollection(result);
+}
+
+/** The resulting entrySet is not "backed" by the Map, so we keep it unmodifiable. */
+@Override
+public Set<Entry<K, V>> entrySet() {
+  final Set<Entry<K, V>> result = new HashSet<>();
+  // Done populate the set
+
+  for (int i = 0; i < DEFAULT_TABLE_SIZE; i++){
+    final Iterator<Entry<K, V>> currChain = table.get(i).iterator();
+    while (currChain.hasNext()) {
+      final Entry<K, V> entry = currChain.next();
+      result.add(entry);
+    }
+
+  }
+
+  return Collections.unmodifiableSet(result);
+}
+
+@Override
+public String toString() {
+  // TODO return the string representation of the underlying table
+  return "";
+}
+
+public boolean equals(final Object that) {
+  if (this == that) {
+    return true;
+  } else if (!(that instanceof Map)) {
+    return false;
+  } else {
+    // TODO simply compare the entry sets
+    return false;
+  }
+}
+
+private int calculateIndex(final Object key) {
+  // positive remainder (as opposed to %)
+  // required in case hashCode is negative!
+  return Math.floorMod(key.hashCode(), table.size());
+}
 }
